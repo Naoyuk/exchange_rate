6057ed1b8116173694f9d2dda4a27ad5eacdc751
Naoyuki Ishida <39.ishida@gmail.com>
2021-05-04 02:45:24 -0700

    install bootstrap

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,2 +1,13 @@
 class Rate < ApplicationRecord
+    def usd_jpy(rate)
+        (rate.jpy / rate.usd).round(2)
+    end
+
+    def cad_jpy(rate)
+        (rate.jpy / rate.cad).round(2)
+    end
+
+    def eur_jpy(rate)
+        (rate.jpy / rate.eur).round(2)
+    end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,9 @@
 set :output, "log/crontab.log"
 set :environment, ENV['RAILS_ENV']
+env :PATH, ENV['PATH']
+set :job_template, "/bin/zsh -l -c ':job'"
+job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
 every 1.hour do
-  rake "exchange:fetch"
+  rake "fetch"
 end
